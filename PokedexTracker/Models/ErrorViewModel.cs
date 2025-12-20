5dbc4ee3e3bb376e0341e46152a8da1ba9d5d4c2
speedshadow59 <laurispielikys@gmail.com>
2025-12-20 02:16:56 +0000

    Delete PokedexTracker/Models directory

--- PokedexTracker/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace PokedexTracker.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
