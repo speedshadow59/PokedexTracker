df8c5d92772fc838c013ec07e1c0aec2e5fcd03e
speedshadow58 <l.pielikys@gmail.com>
2026-01-04 16:09:08 +0000

    Remove unused admincheck2 endpoint

--- api/admincheck2/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ok: true, endpoint: 'admincheck2' })
-  };
-};
